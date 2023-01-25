237b4d0fbbfd49aed3455ca0cd2a410ff46d26bf
Ilya Dubinsky <dubinsky.ilya@gmail.com>
2023-01-25 11:19:34 +0200

    Added decryption, cleaned up

--- java/src/org/ilyadubinsky/cfpp/crypto/AsymmetricAlgorithms.java
+++ java/src/org/ilyadubinsky/cfpp/crypto/AsymmetricAlgorithms.java
@@ -4,8 +4,10 @@ import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
 
 import javax.crypto.BadPaddingException;
@@ -13,9 +15,6 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
-import lombok.extern.java.Log;
-
-@Log
 public class AsymmetricAlgorithms {
 
 	public static byte[] encryptRSA(byte[] data, BigInteger n, BigInteger e)
@@ -29,12 +28,29 @@ public class AsymmetricAlgorithms {
 		KeyFactory kf = KeyFactory.getInstance(Constants.RSA_ALGORITHM);
 		PublicKey rsaPublicKey = kf.generatePublic(rsaPublicKeySpec);
 		
-		/* instantiate the cipher */
+		/* instantiate and initialize the cipher */
 		Cipher c = Cipher.getInstance(Constants.RSA_ALGORITHM);
-
 		c.init(Cipher.ENCRYPT_MODE, rsaPublicKey);
 		
+		/* do the encryption */
 		return c.doFinal(data);
 
+	}
+
+	public static byte[] decryptRSA(byte[] data, BigInteger n, BigInteger e, BigInteger d) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		// TODO: input validation
+
+		/* wrap the key */
+		RSAPrivateKeySpec rsaPrivateKeySpec = new RSAPrivateKeySpec(n, d);
+		
+		KeyFactory kf = KeyFactory.getInstance(Constants.RSA_ALGORITHM);
+		PrivateKey rsaPrivateKey = kf.generatePrivate(rsaPrivateKeySpec);
+		
+		/* instantiate and initialized the cipher */
+		Cipher c = Cipher.getInstance(Constants.RSA_ALGORITHM);
+		c.init(Cipher.DECRYPT_MODE, rsaPrivateKey);
+		
+		/* do the decryption */
+		return c.doFinal(data);
 	}
 }
