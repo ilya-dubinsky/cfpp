1a5b3e9c8eb5eb473ec6d85e6e2fae37341c60d8
idubinsky <dubinsky.ilya@gmail.com>
2023-03-26 14:41:41 +0000

    Removed unnecessary pragma

--- c/src/jose.c
+++ c/src/jose.c
@@ -297,7 +297,6 @@ static char* jose_encrypt_payload(uint8_t payload_enc_alg, uint8_t *cek,
 			/* Compute the 64-bit length of the AAD */
 
 			/* The following is fine but prompts a warning */
-#pragma GCC diagnostic ignored "-Wunsequenced"
 			aad_len_64 = htonll(aad_len_64);
 
 			print_array("\t\tAAD length: ", (uint8_t*) &aad_len_64, HMAC_AAD_LEN_SIZE, "\n");
